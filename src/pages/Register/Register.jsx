e9314b0207b79ede4e868cbaa9a43a93de5b58ef
ikxyz <40415727+Ikxyz@users.noreply.github.com>
2021-07-08 18:08:43 +0100

    update register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Logo from "../../components/Logo/Logo";
 import Button from "../../components/Button/Button";
 import style from "./Register.module.scss";
@@ -6,9 +7,23 @@ import { Link } from "react-router-dom";
 
 export default function Register() {
 
+     const [user, setUser] = useState({ name: "", username: "", password: "", confirmPassword: "" });
+
+     function register(formEvent) {
+          formEvent.preventDefault();
+     }
+
+     function onChange(e) {
+          const name = e.target.name;
+          const value = e.target.value;
+          const data = { ...user, [name]: value };
+          setUser(data);
+     }
 
 
-     return <form  >
+
+     return <form onSubmit={register}>
+          <h1>{JSON.stringify(user)}</h1>
           <div className="container">
                <div className="">
 
@@ -19,16 +34,16 @@ export default function Register() {
 
 
                     <label htmlFor="name">Name</label>
-                    <input type="text" name="name" id="name" datatype="name" required />
+                    <input type="text" name="name" id="name" onChange={onChange} datatype="name" required />
 
                     <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username" datatype="username" required />
+                    <input type="text" name="username" id="username" onChange={onChange} datatype="username" required />
 
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" datatype="password" required />
+                    <input type="password" name="password" id="password" onChange={onChange} datatype="password" required />
 
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="password" name="confirmPassword" id="confirmPassword" datatype="password" required />
+                    <label htmlFor="confirmPassword">Confirm Password  {user.password !== user.confirmPassword ? <strong style={{ color: "red" }}>Password does not match</strong> : ''}</label>
+                    <input type="password" name="confirmPassword" id="confirmPassword" onChange={onChange} datatype="password" required />
 
                     <Button text="Creat Account" isSubmitButton={true} />
 
--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import style from "./Splash.module.scss";
 import Logo from "../../components/Logo/Logo";
+
+
 export default function Splash() {
 
+     const history = useHistory();
+
+     useEffect(() => {
+          setTimeout(() => {
+               // window.location.href = "./login";
+               history.push("/login", { name: "Jane Doe" });
+          }, 3000)
+     }, [])
 
      return <>
           <div className={style.center}>
